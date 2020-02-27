f4ed8a4176d967dbf9534f4640d6822c766d170a
Matti Korttila <matti.korttila@sysart.fi>
2020-02-27 08:01:45 +0200

    Updates

---
+++ precache-manifest.dfc849d4ee4858bf2d56980d5a918280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3f1504dbf86142b985782401053288",
+    "url": "/nostokisat-example/index.html"
+  },
+  {
+    "revision": "f4147fa049753185fba2",
+    "url": "/nostokisat-example/static/css/main.29a6b099.chunk.css"
+  },
+  {
+    "revision": "0c9752af604d30331cb0",
+    "url": "/nostokisat-example/static/js/2.6a4f3681.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nostokisat-example/static/js/2.6a4f3681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4147fa049753185fba2",
+    "url": "/nostokisat-example/static/js/main.1c6e8f12.chunk.js"
+  },
+  {
+    "revision": "583fe6af3c78466106ba",
+    "url": "/nostokisat-example/static/js/runtime-main.d819d755.js"
+  }
+]);
